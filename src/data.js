310cfaebe0d389aef253a9c158a4d4b9de9f5d7e
marvin19 <marita.vindedal@highsoft.com>
2023-10-22 18:58:28 +0200

    Added words for the rest of the year.

--- src/data.js
+++ src/data.js
@@ -7,6 +7,42 @@ export const ord = [
         week: 43,
         word: 'fryktlaus',
         link: 'https://ordbokene.no/nn/22516'
+    }, {
+        week: 44,
+        word: 'fanget',
+        link: 'https://ordbokene.no/nn/22516'
+    }, {
+        week: 45,
+        word: 'å kviskre',
+        link: 'https://ordbokene.no/nn/43660'
+    }, {
+        week: 46,
+        word: 'å lirka',
+        link: 'https://ordbokene.no/nn/45826'
+    }, {
+        week: 47,
+        word: 'kjærleik',
+        link: 'https://ordbokene.no/nn/38327'
+    }, {
+        week: 48,
+        word: 'blåbærlyng',
+        link: 'https://ordbokene.no/nn/7159'
+    }, {
+        week: 49,
+        word: 'sødme',
+        link: 'https://ordbokene.no/bm/59242'
+    }, {
+        week: 50,
+        word: 'sildre',
+        link: 'https://ordbokene.no/nn/65752'
+    }, {
+        week: 51,
+        word: 'gebursdag',
+        link: 'https://ordbokene.no/nn/24527'
+    }, {
+        week: 52,
+        word: 'julefred',
+        link: 'https://ordbokene.no/nn/125030'
     }
 ]
 
