7452544c5d7de9d924ca0b6668fc318fba28b66a
marvin19 <marita.vindedal@highsoft.com>
2023-10-26 20:59:58 +0200

    Removed unecessary hooks.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,13 @@
-import { useState, useEffect } from 'react'
-import { getISODay, getWeek } from 'date-fns';
 import words from './data';
 import WeekWord from './WeekWord';
 import './App.css'
 
 function App() {
-  const [currentWord, setCurrentWord] = useState('');
-
   // Getting current week
-  const currentWeek =  getWeek(new Date());
+  const currentWeek =  51; //getWeek(new Date());
 
   // Finding word based on current week
   const weekObj = words.find(word => word.week === currentWeek);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      const isoDay = getISODay(new Date());
-
-      // Show a new word every monday (ISO day 1);
-      if(isoDay === 1){
-        setCurrentWord(weekObj.word);
-      }
-    }, 1000 * 60 * 60 * 12); // Check every 12 hours
-
-    return() => clearInterval(intervalId);
-  }, [])
 
   return (
     <div>
