2a8d81265e1622895f1b3962fb90dcf57e07dce5
marvin19 <marita.vindedal@highsoft.com>
2023-10-26 21:02:11 +0200

    Fixed mistakes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { getWeek } from 'date-fns'
 import words from './data';
 import WeekWord from './WeekWord';
 import './App.css'
 
+
 function App() {
   // Getting current week
-  const currentWeek =  51; //getWeek(new Date());
+  const currentWeek =  getWeek(new Date());
 
   // Finding word based on current week
   const weekObj = words.find(word => word.week === currentWeek);
