c704be04163a017b3ee4aa2ccd177b79b65a0d4d
marvin19 <marita.vindedal@highsoft.com>
2024-01-10 18:17:15 +0100

    Added more data for 2024.

--- src/data.ts
+++ src/data.ts
@@ -1,55 +1,90 @@
-export interface Word {
-    week: number;
-    word: string;
-    link: string;
+export const ord: Object = {
+    2023: [
+        {
+            week: 42,
+            word: 'å lista',
+            link: 'https://ordbokene.no/nn/45867'
+        }, {
+            week: 43,
+            word: 'fryktlaus',
+            link: 'https://ordbokene.no/nn/22516'
+        }, {
+            week: 44, // current week
+            word: 'fanget',
+            link: 'https://ordbokene.no/bm/13786'
+        }, {
+            week: 45,
+            word: 'å kviskre',
+            link: 'https://ordbokene.no/nn/43660'
+        }, {
+            week: 46,
+            word: 'å lirka',
+            link: 'https://ordbokene.no/nn/45826'
+        }, {
+            week: 47,
+            word: 'kjærleik',
+            link: 'https://ordbokene.no/nn/38327'
+        }, {
+            week: 48,
+            word: 'blåbærlyng',
+            link: 'https://ordbokene.no/nn/7159'
+        }, {
+            week: 49,
+            word: 'sødme',
+            link: 'https://ordbokene.no/bm/59242'
+        }, {
+            week: 50,
+            word: 'sildre',
+            link: 'https://ordbokene.no/nn/65752'
+        }, {
+            week: 51,
+            word: 'gebursdag',
+            link: 'https://ordbokene.no/nn/24527'
+        }, {
+            week: 52,
+            word: 'julefred',
+            link: 'https://ordbokene.no/nn/125030'
+        }
+    ],
+    2024: [
+        {
+            week: 1,
+            word: 'kjensle',
+            link: 'https://ordbokene.no/nn/38149'
+        }, {
+            week: 2,
+            word: 'sutlaus',
+            link: 'https://ordbokene.no/nn/75713'
+        }, {
+            week: 3,
+            word: 'framdriftsplan',
+            link: 'https://ordbokene.no/nn/21555'
+        }, {
+            week: 4,
+            word: 'snodig',
+            link: 'https://ordbokene.no/nn/71165'
+        }, {
+            week: 5,
+            word: 'pønske',
+            link: 'https://ordbokene.no/nn/59703'
+        }, {
+            week: 6,
+            word: 'ubeleilig',
+            link: 'https://ordbokene.no/bm/63646'
+        }, {
+            week: 7,
+            word: 'julefred',
+            link: 'https://ordbokene.no/nn/125030'
+        }, {
+            week: 8,
+            word: 'høfleg',
+            link: 'https://ordbokene.no/nn/32478'
+        }, {
+            week: 9,
+            word: 'larm',
+            link: 'https://ordbokene.no/nn/44668'
+        }
+    ]
 };
-
-export const ord: Word[] = [
-    {
-        week: 42,
-        word: 'å lista',
-        link: 'https://ordbokene.no/nn/45867'
-    }, {
-        week: 43,
-        word: 'fryktlaus',
-        link: 'https://ordbokene.no/nn/22516'
-    }, {
-        week: 44, // current week
-        word: 'fanget',
-        link: 'https://ordbokene.no/bm/13786'
-    }, {
-        week: 45,
-        word: 'å kviskre',
-        link: 'https://ordbokene.no/nn/43660'
-    }, {
-        week: 46,
-        word: 'å lirka',
-        link: 'https://ordbokene.no/nn/45826'
-    }, {
-        week: 47,
-        word: 'kjærleik',
-        link: 'https://ordbokene.no/nn/38327'
-    }, {
-        week: 48,
-        word: 'blåbærlyng',
-        link: 'https://ordbokene.no/nn/7159'
-    }, {
-        week: 49,
-        word: 'sødme',
-        link: 'https://ordbokene.no/bm/59242'
-    }, {
-        week: 50,
-        word: 'sildre',
-        link: 'https://ordbokene.no/nn/65752'
-    }, {
-        week: 51,
-        word: 'gebursdag',
-        link: 'https://ordbokene.no/nn/24527'
-    }, {
-        week: 52,
-        word: 'julefred',
-        link: 'https://ordbokene.no/nn/125030'
-    }
-]
 
 export default ord;
